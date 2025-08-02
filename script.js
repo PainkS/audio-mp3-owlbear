96eee9b54c8689773b4e5c95fd795674f70c4537
PainkS <marcelomalta009@gmail.com>
2025-08-02 19:09:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function tocarAudio() {
   const link = document.getElementById("mp3link").value;
   const player = document.getElementById("audioPlayer");
+
+  if (!link.endsWith(".mp3")) {
+    alert("O link precisa terminar com .mp3");
+    return;
+  }
+
+  player.pause();
   player.src = link;
-  player.play();
+  player.load();
+  player.oncanplay = () => {
+    player.play().catch(err => {
+      console.error("Erro ao tentar tocar:", err);
+      alert("O navegador impediu a reprodução automática. Clique no player.");
+    });
+  };
 }
